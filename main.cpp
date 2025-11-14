a7f99e74941e9eb5f5c80a7f48b448b49130d473
queenxiquec <phamthingocquynh1409@gmail.com>
2025-11-13 20:08:54 -0800

    Refactor heap management and add union/intersect features
    Restored and updated the main application structure, including heap menu options and functionality for max and min heaps. Added union and intersection features for heaps.

--- main.cpp
+++ main.cpp
@@ -7,9 +7,10 @@ using namespace std;
 void HeapMenu();
 void maxHeap();
 void minHeap();
+void UnionAndIntersect();
 
 int main() {
-	do {
+    do {
         system("cls");
         cout << "\n\t\tCMPR131 Chapter 11: Heaps by Khang Quach, Thi Ho, Thi Pham";
         cout << "\n\t\t" << string(70, char(205));
@@ -23,7 +24,7 @@ int main() {
         {
         case 0: exit(1);
         case 1: HeapMenu();  break;
-        case 2:  break;
+        case 2: UnionAndIntersect(); break;
         case 3:
         {
 
@@ -35,7 +36,7 @@ int main() {
         cout << "\n\n";
         system("pause");
 
-	} while (true);
+    } while (true);
 }
 
 void HeapMenu() {
@@ -60,8 +61,8 @@ void HeapMenu() {
         cout << "\n\n";
         system("pause");
     } while (true);
-   
-   
+
+
 }
 
 void maxHeap() {
@@ -109,11 +110,11 @@ void maxHeap() {
             max.push(value);
             break;
         }
-            
+
         case 4:
             cout << "\n\t\tThe first element of the heap: " << max.front();
             break;
-        case 5: 
+        case 5:
             if (max.empty())
             {
                 cout << "\n\t\tThe heap is empty.";
@@ -137,7 +138,8 @@ void maxHeap() {
     } while (true);
 }
 
-void minHeap() {
+void minHeap()
+{
     MinHeap<int> min;
     do {
         system("cls");
@@ -210,33 +212,255 @@ void minHeap() {
     } while (true);
 }
 
-void UnionAndIntersect() {
+
+void UnionAndIntersect()
+{
     do {
         system("cls");
         cout << "\n\t\t2> Union and intersect heap";
         cout << "\n\t\t" << string(70, char(205));
-        cout << "\n\t\t\tA> Union two max Heaps";
-        cout << "\n\t\t\tB> Intersect two max Heaps";
-        cout << "\n\t\t\tC> Union two min Heaps";
-        cout << "\n\t\t\tD> Intersect two min Heaps";
+        cout << "\n\t\t\tA> Union two max Heaps (random)";
+        cout << "\n\t\t\tB> Intersect two max Heaps (random)";
+        cout << "\n\t\t\tC> Union two min Heaps (random)";
+        cout << "\n\t\t\tD> Intersect two min Heaps (random)";
         cout << "\n\t\t" << string(70, char(196));
         cout << "\n\t\t\t0> Exit";
         cout << "\n\t\t" << string(70, char(205)) << '\n';
 
         switch (toupper(inputChar("\n\t\tOption: ", static_cast<string>("ABCD0"))))
         {
-        case '0': return;
+        case '0':
+            return;
+
+            // =========================
+            // A) UNION OF TWO MAX HEAPS (FULLY RANDOM)
+            // =========================
         case 'A':
         {
+            MaxHeap<int> h1, h2;
+
+            int n1 = rand() % 8 + 3;   // size 3..10
+            int n2 = rand() % 8 + 3;   // size 3..10
+
+            // random fill first heap
+            for (int i = 0; i < n1; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;   // 1..100
+                } while (h1.find(value));        // unique within h1
+                h1.push(value);
+            }
+
+            // random fill second heap
+            for (int i = 0; i < n2; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h2.find(value));        // unique within h2
+                h2.push(value);
+            }
+
+            MaxHeap<int> uni = h1;
+            MaxHeap<int> temp = h2;
+
+            while (!temp.empty())
+            {
+                int x = temp.front();
+                temp.pop();
+                if (!uni.find(x))
+                    uni.push(x);
+            }
+
+            cout << "\n\t\tFIRST max heap (size = " << n1 << "): ";
+            if (h1.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h1;
+
+            cout << "\n\n\t\tSECOND max heap (size = " << n2 << "): ";
+            if (h2.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h2;
+
+            cout << "\n\n\t\tUNION (max heap): ";
+            if (uni.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << uni;
+
             break;
         }
-        case 'B': break;
-        case 'D': break;
-        default: cout << "\n\tERROR: invalud option.\n";
+
+        // ==============================
+        // B) INTERSECTION OF TWO MAX HEAPS (FULLY RANDOM)
+        // ==============================
+        case 'B':
+        {
+            MaxHeap<int> h1, h2;
+
+            int n1 = rand() % 8 + 3;   // 3..10
+            int n2 = rand() % 8 + 3;   // 3..10
+
+            // random fill first heap
+            for (int i = 0; i < n1; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h1.find(value));
+                h1.push(value);
+            }
+
+            // random fill second heap
+            for (int i = 0; i < n2; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h2.find(value));
+                h2.push(value);
+            }
+
+            MaxHeap<int> inter;
+            MaxHeap<int> temp = h1;
+
+            while (!temp.empty())
+            {
+                int x = temp.front();
+                temp.pop();
+                if (h2.find(x) && !inter.find(x))
+                    inter.push(x);
+            }
+
+            cout << "\n\t\tFIRST max heap (size = " << n1 << "): ";
+            if (h1.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h1;
+
+            cout << "\n\n\t\tSECOND max heap (size = " << n2 << "): ";
+            if (h2.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h2;
+
+            cout << "\n\n\t\tINTERSECTION (max heap): ";
+            if (inter.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << inter;
+
+            break;
+        }
+
+        // =========================
+        // C) UNION OF TWO MIN HEAPS (FULLY RANDOM)
+        // =========================
+        case 'C':
+        {
+            MinHeap<int> h1, h2;
+
+            int n1 = rand() % 8 + 3;   // 3..10
+            int n2 = rand() % 8 + 3;   // 3..10
+
+            // random fill first heap
+            for (int i = 0; i < n1; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h1.find(value));
+                h1.push(value);
+            }
+
+            // random fill second heap
+            for (int i = 0; i < n2; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h2.find(value));
+                h2.push(value);
+            }
+
+            MinHeap<int> uni = h1;
+            MinHeap<int> temp = h2;
+
+            while (!temp.empty())
+            {
+                int x = temp.front();
+                temp.pop();
+                if (!uni.find(x))
+                    uni.push(x);
+            }
+
+            cout << "\n\t\tFIRST min heap (size = " << n1 << "): ";
+            if (h1.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h1;
+
+            cout << "\n\n\t\tSECOND min heap (size = " << n2 << "): ";
+            if (h2.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h2;
+
+            cout << "\n\n\t\tUNION (min heap): ";
+            if (uni.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << uni;
+
+            break;
+        }
+
+        // ==============================
+        // D) INTERSECTION OF TWO MIN HEAPS (FULLY RANDOM)
+        // ==============================
+        case 'D':
+        {
+            MinHeap<int> h1, h2;
+
+            int n1 = rand() % 8 + 3;   // 3..10
+            int n2 = rand() % 8 + 3;   // 3..10
+
+            // random fill first heap
+            for (int i = 0; i < n1; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h1.find(value));
+                h1.push(value);
+            }
+
+            // random fill second heap
+            for (int i = 0; i < n2; ++i)
+            {
+                int value;
+                do {
+                    value = rand() % 100 + 1;
+                } while (h2.find(value));
+                h2.push(value);
+            }
+
+            MinHeap<int> inter;
+            MinHeap<int> temp = h1;
+
+            while (!temp.empty())
+            {
+                int x = temp.front();
+                temp.pop();
+                if (h2.find(x) && !inter.find(x))
+                    inter.push(x);
+            }
+
+            cout << "\n\t\tFIRST min heap (size = " << n1 << "): ";
+            if (h1.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h1;
+
+            cout << "\n\n\t\tSECOND min heap (size = " << n2 << "): ";
+            if (h2.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << h2;
+
+            cout << "\n\n\t\tINTERSECTION (min heap): ";
+            if (inter.empty()) cout << " (empty)";
+            else cout << "\n\t\t" << inter;
+
+            break;
+        }
+
+        default:
+            cout << "\n\tERROR: invalid option.\n";
         }
 
         cout << "\n\n";
         system("pause");
     } while (true);
-
 }
